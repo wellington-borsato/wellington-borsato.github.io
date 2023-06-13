3d63cac80ebb8703dbe7f53db55ca71492a08094
wellington-borsato <136503060+wellington-borsato@users.noreply.github.com>
2023-06-13 15:44:00 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function start() {
+    var img = document.getElementById('img');
+    img.classList.remove('stop');
+ 
+    // if (img.classList.contains('active')) {
+    //     return img.classList.remove('active');
+    // }
+}
